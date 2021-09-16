474cd6509d87312545f18f70449f159851c1afe9
Alex <alexcn711@gmail.com>
2021-09-16 09:00:06 -0600

    Triangle class

---
+++ src/com/evenstar/model/shapes/Triangle.java
@@ -0,0 +1,13 @@
+package com.evenstar.model.shapes;
+
+import com.evenstar.model.Ray;
+
+public class Triangle implements Shape
+{
+
+    @Override
+    public Hit hitByRay(Ray ray, double tMin, double tMax, Hit hit)
+    {
+        return null;
+    }
+}
